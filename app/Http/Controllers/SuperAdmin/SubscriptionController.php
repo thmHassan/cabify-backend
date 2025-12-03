050f05a0f8a1614b249fe394e212f61a63ad36b5
jaya purohit <purohitjaya16@gmail.com>
2025-12-03 12:00:39 +0700

    Stripe keys store functionality

--- app/Http/Controllers/SuperAdmin/SubscriptionController.php
+++ app/Http/Controllers/SuperAdmin/SubscriptionController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Subscription;
 use App\Models\Tenant;
+use App\Models\Setting;
 use Carbon\Carbon;
 
 class SubscriptionController extends Controller
@@ -185,6 +186,63 @@ class SubscriptionController extends Controller
             return response()->json([
                 'success' => 1,
                 'message' => 'Subscription deleted successfully'
+            ]);
+        }
+        catch(\Exception $e){
+            return response()->json([
+                'error' => 1,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function getStripeKeys(){
+        try{
+            $data = Setting::orderBy("id","DESC")->first();
+
+            $stripe_secret = NULL;
+            $stripe_key = NULL;
+            $stripe_webhook_secret = NULL;
+
+            if(isset($data) && $data != NULL){
+                $stripe_secret = $data->stripe_secret;
+                $stripe_key = $data->stripe_key;
+                $stripe_webhook_secret = $data->stripe_webhook_secret;
+            }
+
+            return response()->json([
+                'success' => 1,
+                'data' => [
+                    'stripe_secret' => $stripe_secret,
+                    'stripe_key' => $stripe_key,
+                    'stripe_webhook_secret' => $stripe_webhook_secret,
+                ]
+                ]);
+        }
+        catch(\Exception $e){
+            return response()->json([
+                'error' => 1,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function storeStripeKeys(Request $request){
+        try{
+            $data = Setting::orderBy("id", "DESC")->first();
+
+            if(!isset($data) || $data == NULL){
+                $data = new Setting;
+            }
+
+            $data->stripe_secret = isset($request->stripe_secret) ? $request->stripe_secret : $data->stripe_secret;
+            $data->stripe_key = isset($request->stripe_key) ? $request->stripe_key : $data->stripe_key;
+            $data->stripe_webhook_secret = isset($request->stripe_webhook_secret) ? $request->stripe_webhook_secret : $data->stripe_webhook_secret;
+            $data->save();
+
+            return response()->json([
+                'success' => 1,
+                'message' => 'Stripe data stored successfully'
             ]);
         }
         catch(\Exception $e){
