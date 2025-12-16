3e336b86f1946341b26aaa8cd5af7931555f2342
jaya purohit <purohitjaya16@gmail.com>
2025-12-16 12:33:08 +0700

    Testing

--- app/Http/Controllers/Company/BookingController.php
+++ app/Http/Controllers/Company/BookingController.php
@@ -24,7 +24,7 @@ class BookingController extends Controller
             $matched = null;
             foreach ($records as $rec) {
                 $polygon = json_decode($rec->features, true);
-                $array = json_decode($polygon['geometry']['coordinates'], true);
+                $array = json_decode($polygon['geometry']['coordinates'], true)[0];
                 dd($polygon, $array);
                 if ($this->pointInPolygon($lat, $lng, $array)) {
                     $matched = $rec;
