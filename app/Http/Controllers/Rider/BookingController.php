3f906c8fd3db12f55a25c45f8ddfd619bde574b6
jaya purohit <purohitjaya16@gmail.com>
2026-01-25 17:10:12 +0530

    Error solve

--- app/Http/Controllers/Rider/BookingController.php
+++ app/Http/Controllers/Rider/BookingController.php
@@ -330,6 +330,44 @@ class BookingController extends Controller
         }
     }
 
+    public function pointInPolygon($lat, $lng, $polygon)
+    {
+        if (count($polygon) == 2) {
+            $lng1 = $polygon[0][0];
+            $lat1 = $polygon[0][1];
+
+            $lng2 = $polygon[1][0];
+            $lat2 = $polygon[1][1];
+
+            return (
+                $lat >= min($lat1, $lat2) &&
+                $lat <= max($lat1, $lat2) &&
+                $lng >= min($lng1, $lng2) &&
+                $lng <= max($lng1, $lng2)
+            );
+        }
+
+        $inside = false;
+        $x = $lng;
+        $y = $lat;
+
+        $numPoints = count($polygon);
+        for ($i = 0, $j = $numPoints - 1; $i < $numPoints; $j = $i++) {
+
+            $xi = $polygon[$i][0];
+            $yi = $polygon[$i][1];
+
+            $xj = $polygon[$j][0];
+            $yj = $polygon[$j][1];
+
+            $intersect = (($yi > $y) != ($yj > $y)) &&
+                ($x < ($xj - $xi) * ($y - $yi) / ($yj - $yi) + $xi);
+
+            if ($intersect) $inside = !$inside;
+        }
+        return $inside;
+    }
+
     public function createBooking(Request $request){
         try{
             $request->validate([
