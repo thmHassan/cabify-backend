0fb0377267980d3c534ead32b1dbf9bddbe661b3
jaya purohit <purohitjaya16@gmail.com>
2026-01-25 17:16:05 +0530

    Test error solve

--- routes/api.php
+++ routes/api.php
@@ -351,7 +351,7 @@ Route::group(['middleware' => ['tenant.db']], function () {
         Route::post('/rider/change-bid-status', [RiderBookingController::class, 'changeBidStatus']);
         Route::post('/rider/cancel-ride', [RiderBookingController::class, 'cancelRide']);
         Route::post('/rider/cancel-confirm-ride', [RiderBookingController::class, 'cancelConfirmRide']);
-        Route::post('/rider/calculate-fare', [RiderBookingController::class, 'cancelConfirmRide']);
+        Route::post('/rider/calculate-fare', [RiderBookingController::class, 'calculateFare']);
     });
 });
 
