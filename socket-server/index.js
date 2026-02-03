ccf51fe096585d4c8edc52f6327a97c2eebf418f
miraweetech <miraweetech@gmail.com>
2026-02-03 17:01:22 +0530

    socket cors error

--- socket-server/index.js
+++ socket-server/index.js
@@ -9,7 +9,14 @@ const app = express();
 
 // Enhanced CORS configuration
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5173", "https://backend.cabifyit.com/socket-api"],
+    // origin: function (origin, callback) {
+    //     // Allow requests with no origin (mobile apps, Postman, etc.)
+    //     if (!origin) return callback(null, true);
+
+    //     // Allow all origins (you can restrict this to specific domains)
+    //     callback(null, true);
+    // },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "database", "X-Requested-With"],
     credentials: true,
