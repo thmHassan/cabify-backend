03133992b8ccbef55db7f5842653b62b2d45f08a
jaya purohit <purohitjaya16@gmail.com>
2026-02-08 17:13:22 +0530

    Tenant remove

--- socket-server/index.js
+++ socket-server/index.js
@@ -181,7 +181,7 @@ io.on("connection", (socket) => {
                 dataArray,
                 {
                     headers: {
-                        database: `tenant${dataArray.database}`,
+                        database: `${dataArray.database}`,
                     }
                 }
             );
