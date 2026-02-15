e8b9a5b994dd52e650a988bceea1e942d8165996
jaya purohit <purohitjaya16@gmail.com>
2026-02-15 11:58:37 +0530

    Issue of paxkage purchase

--- app/Models/DriverPackage.php
+++ app/Models/DriverPackage.php
@@ -10,4 +10,14 @@ class DriverPackage extends Model
     use HasFactory;
 
     protected $table = "driver_packages";
+
+     protected $fillable = [
+        'driver_id',
+        'package_type',
+        'start_date',
+        'expire_date',
+        'post_paid_amount',
+        'package_top_up_id',
+        'package_top_up_name',
+    ];
 }
