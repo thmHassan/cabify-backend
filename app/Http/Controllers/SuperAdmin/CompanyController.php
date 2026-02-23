dcd54c6582d36cbe4cf1e1d0e59b380408fab435
jaya purohit <purohitjaya16@gmail.com>
2026-02-23 09:03:40 +0530

    Data store in dispatch_system table

--- app/Http/Controllers/SuperAdmin/CompanyController.php
+++ app/Http/Controllers/SuperAdmin/CompanyController.php
@@ -173,6 +173,146 @@ class CompanyController extends Controller
                     'stripe_payment' => $tenant->stripe_enable == "yes" ? "enable" : "disable",
                 ]);
 
+                DB::table('dispatch_system')->insert([
+                    [
+                        'dispatch_system' => 'auto_dispatch_plot_base',
+                        'priority' => 1,
+                        'steps' => 'immediately_show_on_dispatcher_panel',
+                        'sub_priority' => 1,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'auto_dispatch_plot_base',
+                        'priority' => 1,
+                        'steps' => 'show_only_after_not_selected_in_auto_dispatch_first_try',
+                        'sub_priority' => 2,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'auto_dispatch_plot_base',
+                        'priority' => 1,
+                        'steps' => 'show_only_after_not_selected_in_auto_dispatch_second_try',
+                        'sub_priority' => 3,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'auto_dispatch_plot_base',
+                        'priority' => 1,
+                        'steps' => 'show_only_after_not_selected_in_auto_dispatch_third_try',
+                        'sub_priority' => 4,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'auto_dispatch_plot_base',
+                        'priority' => 1,
+                        'steps' => 'put_in_bidding_panel',
+                        'sub_priority' => 5,
+                        'status' => 'disable',
+                    ],
+
+                    [
+                        'dispatch_system' => 'bidding_fixed_fare_plot_base',
+                        'priority' => 2,
+                        'steps' => 'wait_time_seconds',
+                        'sub_priority' => 1,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'bidding_fixed_fare_plot_base',
+                        'priority' => 2,
+                        'steps' => 'immediately_show_on_dispatcher_panel',
+                        'sub_priority' => 2,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'bidding_fixed_fare_plot_base',
+                        'priority' => 2,
+                        'steps' => 'shows_up_after_first_rejection_or_wait_time_elapsed',
+                        'sub_priority' => 3,
+                        'status' => 'disable',
+                    ],
+
+                    [
+                        'dispatch_system' => 'auto_dispatch_nearest_driver',
+                        'priority' => 3,
+                        'steps' => 'immediately_show_on_dispatcher_panel',
+                        'sub_priority' => 1,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'auto_dispatch_nearest_driver',
+                        'priority' => 3,
+                        'steps' => 'show_only_after_not_selected_in_auto_dispatch_first_try',
+                        'sub_priority' => 2,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'auto_dispatch_nearest_driver',
+                        'priority' => 3,
+                        'steps' => 'show_only_after_not_selected_in_auto_dispatch_second_try',
+                        'sub_priority' => 3,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'auto_dispatch_nearest_driver',
+                        'priority' => 3,
+                        'steps' => 'show_only_after_not_selected_in_auto_dispatch_third_try',
+                        'sub_priority' => 4,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'auto_dispatch_nearest_driver',
+                        'priority' => 3,
+                        'steps' => 'put_in_bidding_panel',
+                        'sub_priority' => 5,
+                        'status' => 'disable',
+                    ],
+
+                    [
+                        'dispatch_system' => 'manual_dispatch_only',
+                        'priority' => 4,
+                        'steps' => 'manual_only',
+                        'sub_priority' => null,
+                        'status' => 'disable',
+                    ],
+
+                    [
+                        'dispatch_system' => 'bidding',
+                        'priority' => 5,
+                        'steps' => 'immediately_show_on_dispatcher_panel',
+                        'sub_priority' => 1,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'bidding',
+                        'priority' => 5,
+                        'steps' => 'if_not_received_bid_in_first_10_seconds',
+                        'sub_priority' => 2,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'bidding',
+                        'priority' => 5,
+                        'steps' => 'show_only_after_not_selected_in_auto_dispatch_first_try',
+                        'sub_priority' => 3,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'bidding',
+                        'priority' => 5,
+                        'steps' => 'show_only_after_not_selected_in_auto_dispatch_second_try',
+                        'sub_priority' => 4,
+                        'status' => 'disable',
+                    ],
+                    [
+                        'dispatch_system' => 'bidding',
+                        'priority' => 5,
+                        'steps' => 'show_only_after_not_selected_in_auto_dispatch_third_try',
+                        'sub_priority' => 5,
+                        'status' => 'disable',
+                    ],
+                ]);
+
                 // $centralDocuments = \DB::connection('central')->table('document_types')->get();
 
                 // foreach ($centralDocuments as $doc) {
